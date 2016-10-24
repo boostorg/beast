acde9b4978060236f583ad67a7751eb70c91f6be
Vinnie Falco <vinnie.falco@gmail.com>
2016-10-24 18:41:25 -0400

    Add permessage-deflate WebSocket extension:
    This implements the permessage-deflate WebSocket
    extension as described in HyBi Working Group
    draft-ietf-hybi-permessage-compression-28:
    https://tools.ietf.org/html/draft-ietf-hybi-permessage-compression-28
    
    This extension allows messages to be compressed using
    the raw "deflate" algorithm described in RFC 1951,
    "DEFLATE Compressed Data Format Specification version 1.3":
    https://www.ietf.org/rfc/rfc1951.txt

--- test/websocket/stream.cpp
+++ test/websocket/stream.cpp
@@ -802,7 +802,7 @@ public:
         using boost::asio::buffer;
         static std::size_t constexpr limit = 200;
         std::size_t n;
-        for(n = 0; n < limit; ++n)
+        for(n = 0; n <= limit; ++n)
         {
             stream<test::fail_stream<socket_type>> ws(n, ios_);
             auto const restart =
