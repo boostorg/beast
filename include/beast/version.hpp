97bda6f7b592675243ffe36071cc47c8ccff9d69
Vinnie Falco <vinnie.falco@gmail.com>
2017-06-04 21:15:04 -0700

    Set version to 49

--- include/beast/version.hpp
+++ include/beast/version.hpp
@@ -20,7 +20,7 @@
     This is a simple integer that is incremented by one every time
     a set of code changes is merged to the master or develop branch.
 */
-#define BEAST_VERSION 48
+#define BEAST_VERSION 49
 
 #define BEAST_VERSION_STRING "Beast/" BEAST_STR(BEAST_VERSION)
 
