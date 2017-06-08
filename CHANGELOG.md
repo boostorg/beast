eb35b9212a7148383daa8e2ac5b834429eace9df
Niklas Schnelle <niklas@komani.de>
2017-06-08 10:07:52 +0200

    Fix file_body::get() not setting the more flag correctly
    The check for more data actually checked if the end of data was reached
    so it was logically inverted.
    
    This fixes #434

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,7 @@ Version 51
 
 * Fix operator<< for header
 * Tidy up file_body
+* Fix file_body::get() not setting the more flag correctly
 
 API Changes:
 
--- examples/file_body.hpp
+++ examples/file_body.hpp
@@ -88,7 +88,7 @@ struct file_body
             }
             BOOST_ASSERT(nread != 0);
             offset_ += nread;
-            return {{const_buffers_type{buf_, nread}, offset_ >= size_}};
+            return {{const_buffers_type{buf_, nread}, offset_ < size_}};
         }
     };
 };
