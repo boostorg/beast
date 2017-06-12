0f54bc72a47728798d9e4f6f71a409a4d6df5fd3
Vinnie Falco <vinnie.falco@gmail.com>
2017-06-12 15:26:30 -0700

    Add provisional IANA header field names
    fix #473

--- scripts/make_field.sh
+++ scripts/make_field.sh
@@ -4,7 +4,7 @@ export LC_COLLATE=C
 
 echo "// string constants"
 echo '            "<unknown-field>",'
-cat $1 | uniq | sort -f | sed 's/^/            \"/; s/$/\",/'
+cat $1 | sort -f | uniq | sed 's/^/            \"/; s/$/\",/'
 echo
 
 echo "enum class field : unsigned short"
@@ -12,11 +12,11 @@ echo "{"
 echo "    unknown = 0,"
 echo
 #cat $1 | uniq | sort -f | sed 's/./\L&/g; s/^/\t/; s/$/,/'
-cat $1 | uniq | sort -f | sed 's/\(.*\)/    \L\1,/; s/-/_/g'
+cat $1 | sort -f | uniq | sed 's/\(.*\)/    \L\1,/; s/-/_/g'
 echo "};"
 echo
 
 echo "// pairs"
 #cat $1 | uniq | sort -f | sed 's/\(.*\)/\tmatch\(field::\L\1, \"\E\1\"\);/; s/-/_/'
-cat $1 | uniq | sort -f | perl -nE 'chomp; $a=lc($_); $a=~s/-/_/g; say "        match(field::$a, \"$_\");";' | tr -d "\015"
+cat $1 | sort -f | uniq | perl -nE 'chomp; $a=lc($_); $a=~s/-/_/g; say "        match(field::$a, \"$_\");";' | tr -d "\015"
 
