0822e1bb5e8000ab4df7fd09a31e16912254b8d4
Vinnie Falco <vinnie.falco@gmail.com>
2019-01-09 18:24:54 -0800

    Set version to 202

--- include/boost/beast/version.hpp
+++ include/boost/beast/version.hpp
@@ -20,7 +20,7 @@
     This is a simple integer that is incremented by one every
     time a set of code changes is merged to the develop branch.
 */
-#define BOOST_BEAST_VERSION 201
+#define BOOST_BEAST_VERSION 202
 
 #define BOOST_BEAST_VERSION_STRING "Boost.Beast/" BOOST_STRINGIZE(BOOST_BEAST_VERSION)
 
