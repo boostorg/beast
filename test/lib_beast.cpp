4f0d6a4a55262c402510947dd754f341e733da99
Vinnie Falco <vinnie.falco@gmail.com>
2019-03-02 09:12:54 -0800

    tests use lib_beast for cmake

--- test/lib_beast.cpp
+++ test/lib_beast.cpp
@@ -7,9 +7,17 @@
 // Official repository: https://github.com/boostorg/beast
 //
 
-// This file is used to build the asio stbeastatic library,
-// used with BOOST_BEAST_SPLIT_COMPILATION which helps
-// reduce compilation time
+// This file is used to build a static library with
+// asio and beast definitions, to reduce compilation time.
 
+// BOOST_ASIO_SEPARATE_COMPILATION for asio
+#include <boost/asio/impl/src.hpp>
+#include <boost/asio/ssl/impl/src.hpp>
+
+// BOOST_BEAST_SPLIT_COMPILATION for beast
 #include <boost/beast/src.hpp>
 //#include <boost/beast/ssl/impl/src.hpp>
+
+#ifdef BOOST_BEAST_INCLUDE_TEST_MAIN
+#include <boost/beast/_experimental/unit_test/main.cpp>
+#endif
