boostorg/beast

commit d41b7c23288ebe8c2911feb9ffc26b218d539b53

Use memcpy to avoid putting one byte at a time when copying blocks
`memcpy` is likely to be faster than the naive method due to the
possibility of use of vector instructions and copying more than a byte
at a time.

Signed-off-by: Damian Jarek <damian.jarek93@gmail.com>

include/boost/beast/zlib/detail/deflate_stream.ipp
@@ -1298,9 +1298,8 @@ copy_block(
         put_short((std::uint16_t)len);
         put_short((std::uint16_t)~len);
     }
-    // VFALCO Use memcpy?
-    while (len--)
-        put_byte(*buf++);
+    std::memcpy(&pending_buf_[pending_], buf, len);
+    pending_ += len;
 }
 
 //------------------------------------------------------------------------------

commit b972fcb69e2583a6d212da09d3a1bffae3e97c04

Fixed null pointer ASAN error introduced in commit d41b7c23
Signed-off-by: AeroStun <24841307+AeroStun@users.noreply.github.com>

include/boost/beast/zlib/detail/deflate_stream.ipp
@@ -467,7 +467,7 @@ doWrite(z_params& zs, boost::optional<Flush> flush, error_code& ec)
             else if(flush != Flush::block)
             {
                 /* FULL_FLUSH or SYNC_FLUSH */
-                tr_stored_block((char*)0, 0L, 0);
+                tr_stored_block(nullptr, 0L, 0);
                 /* For a full flush, this empty block will be recognized
                  * as a special marker by inflate_sync().
                  */
@@ -1298,7 +1298,8 @@ copy_block(
         put_short((std::uint16_t)len);
         put_short((std::uint16_t)~len);
     }
-    std::memcpy(&pending_buf_[pending_], buf, len);
+    if(buf)
+        std::memcpy(&pending_buf_[pending_], buf, len);
     pending_ += len;
 }
 