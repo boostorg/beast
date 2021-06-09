7215e2cb3b8778ce34802962a2b02e8fe1edf5af
sdarwin <samuel.d.darwin@gmail.com>
2021-06-09 20:08:45 +0000

    Consolidate drone configs

--- .drone/README.md
+++
@@ -1,3 +0,0 @@
-
-.drone.star and other drone scripts originally based on https://github.com/bfgroup/ci_playground
-
--- .drone/drone.bat
+++ .drone/drone.bat
@@ -2,6 +2,8 @@
 @ECHO ON
 setlocal enabledelayedexpansion
 
+if "%DRONE_JOB_BUILDTYPE%" == "boost" (
+
 echo "============> INSTALL"
 
 SET DRONE_BUILD_DIR=%CD: =%
@@ -12,7 +14,7 @@ SET BOOST_BRANCH=develop
 IF "%DRONE_BRANCH%" == "master" SET BOOST_BRANCH=master
 cp tools\user-config.jam %USERPROFILE%\user-config.jam
 cd ..
-SET GET_BOOST=%DRONE_BUILD_DIR%\tools\get-boost.sh
+SET GET_BOOST=!DRONE_BUILD_DIR!\tools\get-boost.sh
 bash -c "$GET_BOOST $DRONE_BRANCH $DRONE_BUILD_DIR"
 cd boost-root
 call bootstrap.bat
@@ -22,14 +24,16 @@ echo "============> SCRIPT"
 
 echo "Running tests"
 b2 --debug-configuration variant=%VARIANT% cxxstd=%CXXSTD% define=%DEFINE% address-model=%ADDRESS_MODEL% toolset=%TOOLSET% --verbose-test libs/beast/test -j3
-if %errorlevel% neq 0 exit /b %errorlevel%
+if !errorlevel! neq 0 exit /b !errorlevel!
 
 echo "Running libs/beast/example"
 b2 --debug-configuration variant=%VARIANT% cxxstd=%CXXSTD% define=%DEFINE% address-model=%ADDRESS_MODEL% toolset=%TOOLSET% libs/beast/example -j3
-if %errorlevel% neq 0 exit /b %errorlevel%
+if !errorlevel! neq 0 exit /b !errorlevel!
 
 echo "Running run-fat-tests"
 b2 --debug-configuration variant=%VARIANT% cxxstd=%CXXSTD% define=%DEFINE% address-model=%ADDRESS_MODEL% toolset=%TOOLSET% --verbose-test libs/beast/test//run-fat-tests -j3
-if %errorlevel% neq 0 exit /b %errorlevel%
+if !errorlevel! neq 0 exit /b !errorlevel!
 
 echo "============> COMPLETED"
+
+)
