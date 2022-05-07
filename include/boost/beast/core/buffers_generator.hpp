af1a6841476b471e08f0e180982742f803995da8
sehe <sgheeren@gmail.com>
2022-05-07 04:48:47 +0200

    buffers_generator, buffers_generator_test
    async_write buffers_generator test
    
    There was a bug due to using `break` inside a faux-routine which doesn't
    do what C++ semantics suggest (because Duff's device is a switch and
    break means different things in switches).
    
    write, async_write with tests
    
    TODO split sources and tidy includes after
    
    Draft documentation BuffersGenerator/is_buffers_generator

---
+++ include/boost/beast/core/buffers_generator.hpp
@@ -0,0 +1,221 @@
+//
+// Copyright (c) 2020 Vinnie Falco (vinnie dot falco at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/vinniefalco/BeastLounge
+//
+
+#ifndef BOOST_BEAST_CORE_BUFFERS_GENERATOR_HPP
+#define BOOST_BEAST_CORE_BUFFERS_GENERATOR_HPP
+
+#include <boost/beast/core/detail/config.hpp>
+
+#include <boost/asio/write.hpp>
+#include <boost/asio/async_result.hpp>
+#include <boost/asio/compose.hpp>
+#include <boost/asio/coroutine.hpp>
+
+#include <boost/beast/core/buffer_traits.hpp>
+#include <boost/beast/core/stream_traits.hpp>
+#include <boost/beast/core/detail/type_traits.hpp>
+
+#include <boost/throw_exception.hpp>
+#include <type_traits>
+
+namespace boost {
+namespace beast {
+
+/** Determine if type satisfies the <em>BuffersGenerator</em> requirements.
+
+    This metafunction is used to determine if the specified type meets the
+    requirements for a buffers generator.
+
+    The static member `value` will evaluate to `true` if so, `false` otherwise.
+
+    @tparam T a type to check
+*/
+#ifdef BOOST_BEAST_DOXYGEN
+template <class T>
+struct is_buffers_generator
+    : integral_constant<bool, automatically_determined>
+{
+};
+#else
+template<class T, class = void>
+struct is_buffers_generator
+    : std::false_type
+{
+};
+
+template<class T>
+struct is_buffers_generator<
+    T, detail::void_t<decltype(
+        typename T::const_buffers_type(
+            std::declval<T&>().prepare(
+                std::declval<error_code&>())),
+        std::declval<T&>().consume(
+            std::size_t{})
+    )>> : std::true_type
+{
+};
+#endif
+
+//----------------------------------------------------------
+
+namespace detail {
+
+template <
+    class AsyncWriteStream,
+    class BuffersGenerator>
+struct write_buffers_generator_op
+    : boost::asio::coroutine
+{
+    write_buffers_generator_op(AsyncWriteStream& stream,
+                               BuffersGenerator generator)
+        : stream_(stream)
+        , generator_(std::move(generator))
+    {
+    }
+
+    template <class Self>
+    void operator()(Self& self,
+                    error_code ec = {},
+                    std::size_t n = 0)
+    {
+        BOOST_ASIO_CORO_REENTER(*this)
+        {
+            for(;;)
+            {
+                BOOST_ASIO_CORO_YIELD
+                {
+                    auto cb = generator_.prepare(ec);
+                    if (ec)
+                        goto complete;
+                    if (boost::beast::buffer_bytes(cb) == 0)
+                        goto complete;
+                    stream_.async_write_some(
+                        cb, std::move(self));
+                }
+                generator_.consume(n);
+
+                total_ += n;
+            }
+
+        complete:
+            self.complete(ec, total_);
+        }
+    }
+
+private:
+    AsyncWriteStream& stream_;
+    BuffersGenerator generator_;
+    std::size_t total_ = 0;
+};
+
+} // detail
+
+//----------------------------------------------------------
+
+template <
+    class SyncWriteStream,
+    class BuffersGenerator
+#if ! BOOST_BEAST_DOXYGEN
+    ,
+    typename std::enable_if<
+        is_sync_write_stream<SyncWriteStream>::value &&
+        is_buffers_generator<BuffersGenerator>::value>::
+        type* = nullptr
+#endif
+    >
+size_t
+write(SyncWriteStream& stream,
+      BuffersGenerator generator,
+      beast::error_code& ec)
+{
+    ec.clear();
+    size_t total = 0;
+    for (;;) {
+        auto cb = generator.prepare(ec);
+        if (ec)
+            break;
+        if (boost::beast::buffer_bytes(cb) == 0)
+            break;
+
+        size_t n = net::write(stream, cb, ec);
+
+        if (ec)
+            break;
+
+        generator.consume(n);
+        total += n;
+    }
+
+    return total;
+}
+
+//----------------------------------------------------------
+
+template <
+    class SyncWriteStream,
+    class BuffersGenerator
+#if ! BOOST_BEAST_DOXYGEN
+    ,
+    typename std::enable_if<
+        is_sync_write_stream<SyncWriteStream>::value &&
+        is_buffers_generator<BuffersGenerator>::value>::
+        type* = nullptr
+#endif
+    >
+std::size_t
+write(SyncWriteStream& stream, BuffersGenerator generator)
+{
+    beast::error_code ec;
+    std::size_t n = write(stream, generator, ec);
+    if (ec)
+        BOOST_THROW_EXCEPTION(system_error{ ec });
+    return n;
+}
+
+//----------------------------------------------------------
+
+template <
+    class AsyncWriteStream,
+    class BuffersGenerator,
+    class CompletionToken
+#if !BOOST_BEAST_DOXYGEN
+    ,
+    typename std::enable_if<
+        is_async_write_stream<AsyncWriteStream>::value &&
+        is_buffers_generator<BuffersGenerator>::value //
+        >::type* = nullptr
+#endif
+    >
+auto
+async_write(AsyncWriteStream& stream,
+            BuffersGenerator generator,
+            CompletionToken&& token) ->
+    typename net::async_result<
+        typename std::decay<CompletionToken>::type,
+        void(error_code, std::size_t)>::return_type
+{
+    static_assert(
+        is_async_write_stream<AsyncWriteStream>::value,
+        "AsyncWriteStream type requirements not met");
+    return net::async_compose< //
+        CompletionToken,
+        void(error_code, std::size_t)>(
+        detail::write_buffers_generator_op<
+            AsyncWriteStream,
+            BuffersGenerator>{ stream,
+                               std::move(generator) },
+        token, // REVIEW: why doesn't forwarding work here?
+               // SEHE: do we want to take `token` by value?
+        stream);
+}
+
+} // namespace beast
+} // namespace boost
+
+#endif // BOOST_BEAST_CORE_BUFFERS_GENERATOR_HPP
---
+++ test/beast/core/buffers_generator.cpp
@@ -0,0 +1,245 @@
+//
+// Copyright (c) 2016-2019 Vinnie Falco (vinnie dot falco at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/boostorg/beast
+//
+
+// Test that header file is self-contained.
+#include <boost/beast/core/buffers_generator.hpp>
+
+#include "test_buffer.hpp"
+
+#include <boost/asio/error.hpp>
+#include <boost/beast/_experimental/unit_test/suite.hpp>
+#include <boost/beast/core/buffers_suffix.hpp>
+#include <boost/beast/core/detail/static_ostream.hpp>
+#include <boost/beast/core/flat_buffer.hpp>
+#include <boost/beast/_experimental/test/stream.hpp>
+
+#include <iostream>
+
+namespace boost {
+namespace beast {
+
+namespace test_detail {
+
+struct test_buffers_generator {
+    using underlying_buffer_sequence = std::array<net::const_buffer, 2>;
+    using const_buffers_type = buffers_suffix<underlying_buffer_sequence>;
+    size_t iterations_ = 5;
+    bool verbose_ = false;
+
+    test_buffers_generator(bool verbose = false) : verbose_(verbose) {}
+
+    const_buffers_type cur_{};
+
+    const_buffers_type prepare(error_code& ec) {
+        if (verbose_)
+            std::clog
+                << "prepare, iterations_:" << iterations_
+                << " '" << buffers_to_string(cur_) << "' ";
+        if (!buffer_bytes(cur_)) {
+            if (iterations_) {
+                cur_ = const_buffers_type(
+                    underlying_buffer_sequence{
+                        net::buffer("abcde", iterations_),
+                        net::buffer("12345", iterations_),
+                    });
+                iterations_ -= 1;
+            } else {
+                ec = net::error::eof;
+            }
+        }
+
+        if (verbose_)
+            std::clog << " -> '" << buffers_to_string(cur_)
+                      << "'\n";
+        return const_buffers_type{ cur_ };
+    }
+
+    void consume(size_t n) {
+        cur_.consume(n);
+    }
+};
+
+} // namespace test_detail
+
+class buffers_generator_test : public unit_test::suite
+{
+public:
+    using Generator = test_detail::test_buffers_generator;
+
+    void
+    check_minimal_generator()
+    {
+        static_assert(is_buffers_generator<Generator>::value,
+                      "buffers_generator not modeled");
+
+        Generator gen;
+        error_code ec;
+
+        std::vector<std::string> actual;
+        Generator::const_buffers_type b;
+
+        while (buffer_bytes(b = gen.prepare(ec))) {
+            BEAST_EXPECT(!ec);
+            actual.push_back(buffers_to_string(b));
+
+            gen.consume(3); // okay if > buffer_bytes
+        }
+        BEAST_EXPECT(ec == net::error::eof);
+        BEAST_EXPECT(
+            (actual ==
+             std::vector<std::string>{
+                 "abcde12345", "de12345", "2345", "5",
+                 "abcd1234", "d1234", "34", "abc123", "123",
+                 "ab12", "2", "a1" }));
+    }
+
+    void
+    check_write()
+    {
+        net::io_context ioc;
+        test::stream out(ioc), in(ioc);
+        {
+            test::connect(out, in);
+
+            test_detail::test_buffers_generator gen;
+
+            beast::error_code ec;
+            auto total = write(out, gen, ec);
+            BEAST_EXPECT(total == 30);
+            BEAST_EXPECT(ec == net::error::eof);
+        }
+
+        BEAST_EXPECT(5 == out.nwrite());
+        BEAST_EXPECT(0 == out.nwrite_bytes());
+        BEAST_EXPECT(0 == out.nread_bytes());
+        BEAST_EXPECT(0 == out.nread());
+
+        BEAST_EXPECT(30 == in.nwrite_bytes());
+        BEAST_EXPECT("abcde12345abcd1234abc123ab12a1" == in.str());
+    }
+
+    void
+    check_write_exception()
+    {
+        net::io_context ioc;
+        test::stream out(ioc), in(ioc);
+        {
+            test::connect(out, in);
+
+            test_detail::test_buffers_generator gen;
+
+            try {
+                auto total = write(out, gen);
+                BEAST_EXPECT(!"unreachable");
+                BEAST_EXPECT(total==30);
+            } catch (system_error const& se) {
+                BEAST_EXPECT(se.code() == net::error::eof);
+            }
+        }
+
+        BEAST_EXPECT(5 == out.nwrite());
+        BEAST_EXPECT(30 == in.nwrite_bytes());
+        BEAST_EXPECT("abcde12345abcd1234abc123ab12a1" == in.str());
+    }
+
+    void
+    check_async_write()
+    {
+        net::io_context ioc;
+        test::stream out(ioc), in(ioc);
+        {
+            test::connect(out, in);
+
+            test_detail::test_buffers_generator gen;
+
+            async_write(
+                out, gen, [&](error_code ec, size_t total) {
+                    BEAST_EXPECT(total == 30);
+                    BEAST_EXPECT(ec == net::error::eof);
+                });
+
+            ioc.run();
+        }
+
+        BEAST_EXPECT(5 == out.nwrite());
+        BEAST_EXPECT(0 == out.nwrite_bytes());
+        BEAST_EXPECT(0 == out.nread_bytes());
+        BEAST_EXPECT(0 == out.nread());
+
+        BEAST_EXPECT(30 == in.nwrite_bytes());
+        BEAST_EXPECT("abcde12345abcd1234abc123ab12a1" == in.str());
+    }
+
+    void
+    check_write_fail()
+    {
+        net::io_context ioc;
+        test::fail_count fc { 3 };
+        test::stream out(ioc, fc), in(ioc);
+        {
+            test::connect(out, in);
+
+            test_detail::test_buffers_generator gen;
+
+            try {
+                /*auto total =*/ write(out, gen);
+                BEAST_EXPECT(!"unreachable");
+            } catch (system_error const& se) {
+                BEAST_EXPECT(se.code() == test::error::test_failure);
+            }
+        }
+
+        BEAST_EXPECT(3 == out.nwrite());
+        BEAST_EXPECT(18 == in.nwrite_bytes()); // first two writes 10+8
+        BEAST_EXPECT("abcde12345abcd1234" == in.str());
+    }
+
+    void
+    check_async_write_fail()
+    {
+        net::io_context ioc;
+        test::fail_count fc { 3 };
+        test::stream out(ioc, fc), in(ioc);
+        {
+            test::connect(out, in);
+
+            test_detail::test_buffers_generator gen;
+
+            async_write(
+                out, gen, [&](error_code ec, size_t total) {
+                    BEAST_EXPECT(total == 18);
+                    BEAST_EXPECT(ec == test::error::test_failure);
+                });
+
+            ioc.run();
+        }
+
+        BEAST_EXPECT(3 == out.nwrite());
+
+        BEAST_EXPECT(18 == in.nwrite_bytes()); // first two writes 10+8
+        BEAST_EXPECT("abcde12345abcd1234" == in.str());
+    }
+
+    void
+    run() override
+    {
+        check_minimal_generator();
+        check_write();
+        check_write_exception();
+        check_async_write();
+
+        check_write_fail();
+        check_async_write_fail();
+    }
+};
+
+BEAST_DEFINE_TESTSUITE(beast,core,buffers_generator);
+
+} // beast
+} // boost
