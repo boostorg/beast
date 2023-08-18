d6c830e3da999af2ede1b245b86e17a2a73dde16
Klemens Morgenstern <klemens.d.morgenstern@gmail.com>
2023-08-18 15:39:26 +0800

    work_guard has license info.

--- include/boost/beast/core/detail/work_guard.hpp
+++ include/boost/beast/core/detail/work_guard.hpp
@@ -1,3 +1,12 @@
+//
+// Copyright (c) 2020 Richard Hodges (hodges.r@gmail.com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/boostorg/beast
+//
+
 #ifndef BOOST_BEAST_CORE_DETAIL_WORK_GUARD_HPP
 #define BOOST_BEAST_CORE_DETAIL_WORK_GUARD_HPP
 
