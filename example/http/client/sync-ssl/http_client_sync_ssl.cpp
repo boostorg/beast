9bf2184b33313f6c3cfb1bfb6686156e729e9203
Mohammad Nejati <ashtumashtum@gmail.com>
2024-05-28 09:31:17 +0000

    Fix error handling in SSL shutdown operations in examples

--- example/http/client/sync-ssl/http_client_sync_ssl.cpp
+++ example/http/client/sync-ssl/http_client_sync_ssl.cpp
@@ -108,16 +108,26 @@ int main(int argc, char** argv)
         // Gracefully close the stream
         beast::error_code ec;
         stream.shutdown(ec);
-        if(ec == net::error::eof)
-        {
-            // Rationale:
-            // http://stackoverflow.com/questions/25587403/boost-asio-ssl-async-shutdown-always-finishes-with-an-error
-            ec = {};
-        }
-        if(ec)
-            throw beast::system_error{ec};
 
-        // If we get here then the connection is closed gracefully
+        // ssl::error::stream_truncated, also known as an SSL "short read",
+        // indicates the peer closed the connection without performing the
+        // required closing handshake (for example, Google does this to
+        // improve performance). Generally this can be a security issue,
+        // but if your communication protocol is self-terminated (as
+        // it is with both HTTP and WebSocket) then you may simply
+        // ignore the lack of close_notify.
+        //
+        // https://github.com/boostorg/beast/issues/38
+        //
+        // https://security.stackexchange.com/questions/91435/how-to-handle-a-malicious-ssl-tls-shutdown
+        //
+        // When a short read would cut off the end of an HTTP message,
+        // Beast returns the error beast::http::error::partial_message.
+        // Therefore, if we see a short read here, it has occurred
+        // after the message has been completed, so it is safe to ignore it.
+
+        if(ec != net::ssl::error::stream_truncated)
+            throw beast::system_error{ec};
     }
     catch(std::exception const& e)
     {
