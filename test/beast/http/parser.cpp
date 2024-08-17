a9c4a19f26ecbb4942ac21d020b518f8ec9e6930
Mohammad Nejati <ashtumashtum@gmail.com>
2024-08-17 13:05:51 +0000

    parser uses basic_fields::insert() with error_code overload

--- test/beast/http/parser.cpp
+++ test/beast/http/parser.cpp
@@ -327,6 +327,40 @@ public:
     }
 
     void
+    testHeaderFieldLimits()
+    {
+        auto big_field_name  = std::string(fields::max_name_size + 1, 'a');
+        auto big_field_value = std::string(fields::max_value_size + 1, 'a');
+
+        {
+            parser_type<false> p;
+            p.header_limit((std::numeric_limits<std::uint32_t>::max)());
+            error_code ec;
+            flat_buffer b;
+            ostream(b) <<
+                "HTTP/1.1 200 OK\r\n"
+                << big_field_name
+                <<": value\r\n"
+                "\r\n";
+            put(b.data(), p, ec);
+            BEAST_EXPECT(ec == error::header_field_name_too_large);
+        }
+        {
+            parser_type<false> p;
+            p.header_limit((std::numeric_limits<std::uint32_t>::max)());
+            error_code ec;
+            flat_buffer b;
+            ostream(b) <<
+                "HTTP/1.1 200 OK\r\n"
+                << "name: "
+                << big_field_value << "\r\n"
+                << "\r\n";
+            put(b.data(), p, ec);
+            BEAST_EXPECT(ec == error::header_field_value_too_large);
+        }
+    }
+
+    void
     testIssue818()
     {
         // Make sure that the parser clears pre-existing fields
@@ -457,6 +491,7 @@ public:
         testParse();
         testNeedMore<flat_buffer>();
         testNeedMore<multi_buffer>();
+        testHeaderFieldLimits();
         testGotSome();
         testIssue818();
         testIssue1187();
