fb1a12c86f4351905992f8da17feedcb821be490
DockedFerret800 <DockedFerret800@outlook.com>
2025-07-24 13:01:26 +0100

    Add missing return statements to examples

--- example/advanced/server/advanced_server.cpp
+++ example/advanced/server/advanced_server.cpp
@@ -290,7 +290,7 @@ private:
             return;
 
         if(ec)
-            fail(ec, "read");
+            return fail(ec, "read");
 
         // Echo the message
         ws_.text(ws_.got_text());
