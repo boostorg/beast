a303ba6587ad789a604afed4f0238032c702cd92
Mungo Gill <mungo.gill@me.com>
2025-10-03 09:10:09 +0100

    fix repo in copyright notice

--- example/websocket/client/crypto-ai-ssl/historic_price_fetcher.hpp
+++ example/websocket/client/crypto-ai-ssl/historic_price_fetcher.hpp
@@ -4,7 +4,7 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
-// Official repository: https://github.com/cppalliance
+// Official repository: https://github.com/boostorg/beast
 //
 
 #ifndef BOOST_BEAST_EXAMPLE_HISTORIC_PRICE_FETCHER
