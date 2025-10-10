b6c8f5c2ed6a9696072ba4b7145913a5a1cf53a7
Mohammad Nejati <ashtumashtum@gmail.com>
2025-10-10 07:03:59 +0000

    buffer_body ignores empty chunks
    Fixes #3032

--- test/beast/http/buffer_body.cpp
+++ test/beast/http/buffer_body.cpp
@@ -14,6 +14,8 @@
 #include <boost/beast/core/ostream.hpp>
 #include <boost/beast/http/parser.hpp>
 #include <boost/beast/http/read.hpp>
+#include <boost/beast/http/write.hpp>
+#include <boost/beast/http/serializer.hpp>
 
 #include <boost/beast/_experimental/unit_test/suite.hpp>
 #include <boost/beast/_experimental/test/stream.hpp>
@@ -52,9 +54,53 @@ public:
     }
 
     void
+    testIssue3032()
+    {
+        net::io_context ioc;
+        error_code ec;
+        test::stream ts{ioc}, tr{ioc};
+        ts.connect(tr);
+        response<buffer_body> res;
+        response_serializer<buffer_body> sr(res);
+        char buf[3] = {'a', 'b', 'c'};
+
+        res.chunked(true);
+        res.body().data = buf;
+        res.body().size = sizeof(buf);
+        write(ts, sr, ec);
+        BEAST_EXPECTS(
+            ec == error::need_buffer, ec.message());
+        BEAST_EXPECTS(
+            tr.str() == 
+                "HTTP/1.1 200 OK\r\n"
+                "Transfer-Encoding: chunked\r\n"
+                "\r\n"
+                "3\r\nabc\r\n",
+            tr.str()
+        );
+
+        tr.clear();
+        res.body().data = buf;
+        res.body().size = 0;
+        write(ts, sr, ec);
+        BEAST_EXPECTS(
+            ec == error::need_buffer, ec.message());
+        BEAST_EXPECTS(
+            tr.str().empty(), tr.str());
+
+        res.body().more = false;
+        write(ts, sr, ec);
+        BEAST_EXPECT(sr.is_done());
+        BEAST_EXPECTS(! ec, ec.message());
+        BEAST_EXPECTS(
+            tr.str() == "0\r\n\r\n", tr.str());
+    }
+
+    void
     run() override
     {
         testIssue1717();
+        testIssue3032();
     }
 };
 
